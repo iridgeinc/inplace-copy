c3077ee681a23f8561fb61d90b601d6446eaef18
Motohiro Ueki <motohiro.ueki@iridge.jp>
2024-08-08 15:35:48 +0900

    version up modules.

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,18 +1,16 @@
+import type { RollupOptions } from 'rollup';
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
 import typescript from 'rollup-plugin-typescript2'
 import json from '@rollup/plugin-json'
-import { terser } from 'rollup-plugin-terser'
-
-const pkg = require('./package.json')
+import terser from '@rollup/plugin-terser'
 
 const libraryName = 'inplace-copy'
 
-export default {
+const config: RollupOptions = {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: libraryName, format: 'umd', sourcemap: true },
+    { file: "dist/inplace-copy.umd.js", name: libraryName, format: 'umd', sourcemap: true },
     {
       file: `dist/${libraryName}.umd.min.js`,
       name: libraryName,
@@ -20,7 +18,7 @@ export default {
       sourcemap: true,
       plugins: [terser()],
     },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    { file: "dist/inplace-copy.es5.js", format: 'es', sourcemap: true },
     { file: `dist/${libraryName}.es5.min.js`, format: 'es', sourcemap: true, plugins: [terser()] },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
@@ -39,8 +37,7 @@ export default {
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve(),
-
-    // Resolve source maps to the original source
-    sourceMaps(),
   ],
 }
+
+export default config
